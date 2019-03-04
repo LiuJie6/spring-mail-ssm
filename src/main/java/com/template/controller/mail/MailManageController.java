5f643fc0b8b1952d82c16f75915e03f02361af94
liujie <http://10.0.200.18/MIB/parking-log-analysis-platform.git>
2019-03-04 16:20:33 +0800

    发送邮件测试接口更新

--- src/main/java/com/template/controller/mail/MailManageController.java
+++ src/main/java/com/template/controller/mail/MailManageController.java
@@ -3,7 +3,9 @@ package com.template.controller.mail;
 import com.template.model.response.ResponseResult;
 import com.template.model.state.State;
 import com.template.service.manage.mail.api.IMailService;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -26,7 +28,10 @@ public class MailManageController {
     @Resource(name = "mailService")
     private IMailService mailService;
 
-    public Object sendMailTest(String subject, String content, String to) throws Exception {
+    @PostMapping("test")
+    public Object sendMailTest(@RequestParam(value = "subject") String subject
+            , @RequestParam(value = "content") String content
+            , @RequestParam(value = "to") String to) throws Exception {
         this.mailService.sendMail(subject, content, to);
         return new ResponseResult(true, State.SUCCESS.getCode(), "");
     }
